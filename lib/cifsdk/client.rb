987264fcc81a6d29e37685755b3c33e398ec1d08
Wes Young <wes@barely3am.com>
2015-12-15 20:49:49 -0500

    bugfixes

--- lib/cifsdk/client.rb
+++ lib/cifsdk/client.rb
@@ -27,30 +27,29 @@ module CIFSDK
     def _make_request(uri='',type='get',params={})
 
       case type
-      when 'get'
-        self.logger.debug { "uri: " + @remote + "#{uri}" }
-        self.logger.debug { "params: #{params}" }
-
-        uri = URI(@remote + uri)
-        res = @handle.get(uri,params, @headers)
-      when 'put'
-        uri = URI(@remote)
-        self.logger.debug { "uri: #{uri}.to_s" }
-        res = @handle.post(uri,params['data'],@headers)
+        when 'get'
+          self.logger.debug { "uri: " + @remote + "#{uri}" }
+          self.logger.debug { "params: #{params}" }
+          uri = URI(@remote + uri)
+          res = @handle.get(uri,params, @headers)
+        when 'put'
+          uri = URI(@remote)
+          self.logger.debug { "uri: #{uri}.to_s" }
+          res = @handle.post(uri,params['data'],@headers)
       end
 
       case res.status_code
-      when 200...299
-        return JSON.parse(res.body)
-      when 300...399
-        @logger.debug { "received: #{res.status_code}" }
-      when 400
-          @logger.warn { 'unauthorized, bad or missing token' }
-      when 404
-          @logger.warn { 'invalid remote uri: #{uri}.to_s' }
-      when 500...600
-        @logger.fatal { 'router failure, contact administrator' }
-      end
+        when 200...299
+          return JSON.parse(res.body)
+        when 300...399
+          @logger.debug { "received: #{res.status_code}" }
+        when 400
+            @logger.warn { 'unauthorized, bad or missing token' }
+        when 404
+            @logger.warn { 'invalid remote uri: #{uri}.to_s' }
+        when 500...600
+          @logger.fatal { 'router failure, contact administrator' }
+        end
       return nil
 
     end
